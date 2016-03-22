6e085c06686e21aad4efe748469b5daf582cd98e
JeremyVDV <jeremy@live.nl>
2016-03-22 10:31:19 +0100

    #36 - Calculeerfunctie aangemaakt

--- app/src/main/java/com/example/titan/dyscalculator/MainActivity.java
+++ app/src/main/java/com/example/titan/dyscalculator/MainActivity.java
@@ -1,22 +1,22 @@
 package com.example.titan.dyscalculator;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    Calculator calc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Log.d("Uitkomst som", " " + calc.Calculate("15005.623847928374982347+7.52348723486723978"));
     }
 
     @Override
