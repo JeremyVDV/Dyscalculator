0cfb7c17c0ca055f8c070fb6c7f1b66ab1001949
stanrost <stanrost@msn.com>
2016-03-22 10:06:59 +0100

    #sprint1 english names

--- app/src/main/java/com/example/titan/dyscalculator/MainActivity.java
+++ app/src/main/java/com/example/titan/dyscalculator/MainActivity.java
@@ -1,22 +1,47 @@
 package com.example.titan.dyscalculator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button2;
+    TextView tv;
+    String s = "1";
+    HorizontalScrollView sc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        button2 = (Button) findViewById(R.id.button);
+        tv = (TextView) findViewById(R.id.textView);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                s = s + " + 1";
+                tv.setText(s);
+            }
+        });
+       // tv.setMovementMethod(new ScrollingMovementMethod());
+        //sc = (HorizontalScrollView) findViewById(R.id.sc);
+        //sc.postDelayed(new Runnable() {
+       //     public void run() {
+         //       sc.fullScroll(HorizontalScrollView.FOCUS_RIGHT);
+          //  }
+        //}, 100L);
+
     }
 
     @Override
