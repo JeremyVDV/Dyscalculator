b00e132900dfe2c6d46f148072c5cbe5c3142bbc
Jeremy van der Veen <jeremy@live.nl>
2016-05-24 17:57:27 +0200

    Bug fix

--- app/src/main/java/com/example/titan/dyscalculator/MainActivity.java
+++ app/src/main/java/com/example/titan/dyscalculator/MainActivity.java
@@ -503,6 +503,7 @@ public class MainActivity extends AppCompatActivity {
         String combined = "";
         int i = 0;
         for (String split : splitted) {
+            /* Fixes the bug where the zero dissapears
             if(cashMode) {
                 try {
                     split = formatter.format(Double.parseDouble(split));
@@ -511,6 +512,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
             else {
+            */
                 if (split.contains(".")) {
                     String replaced = split.replace(".", ";");
                     String[] dotSplit = replaced.split(";");
@@ -524,7 +526,7 @@ public class MainActivity extends AppCompatActivity {
                         //No exception needed
                     }
                 }
-            }
+            // }
 
             if (i == 0) {
                 combined = split;
