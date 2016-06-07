e2ed786d73ee122a4b822b3835f05a87ae2d94ef
Joris Rijkes <jorisrijkes@gmail.com>
2016-06-07 12:45:56 +0200

    #136_#151 adapterlist begin opzet voor de instellingen.

---
+++ app/src/main/java/com/example/titan/dyscalculator/Setting.java
@@ -0,0 +1,15 @@
+package com.example.titan.dyscalculator;
+
+/**
+ * Created by Joris on 7-6-2016.
+ */
+public class Setting {
+
+    public String setting_name;
+    public String setting_value;
+
+    public Setting(String setting_name, String setting_value) {
+        this.setting_name = setting_name;
+        this.setting_value = setting_value;
+    }
+}
--- app/src/main/java/com/example/titan/dyscalculator/SettingsActivity.java
+++ app/src/main/java/com/example/titan/dyscalculator/SettingsActivity.java
@@ -1,6 +1,5 @@
 package com.example.titan.dyscalculator;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,8 +8,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    SettingsListViewAdapter MyAdapter;
+    ListView ls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +30,18 @@ public class SettingsActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //View v = getLayoutInflater().inflate(R.layout.activity_settings_list_view, );
+        ls = (ListView) findViewById(R.id.settings_list_view);
+
+        MyAdapter = new SettingsListViewAdapter(this, new ArrayList<Setting>());
+
+        Setting setting1 = new Setting("Test", "123");
+        Setting setting2 = new Setting("Test2", "321");
+
+        MyAdapter.settingItems.add(setting1);
+        MyAdapter.settingItems.add(setting2);
+
+        ls.setAdapter(MyAdapter);
     }
 
     @Override
